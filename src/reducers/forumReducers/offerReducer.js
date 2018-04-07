9c05fc182452e98deeae0179905f9d4a31f9c8c2
Rio <rio.nose@smartsparrow.com>
2018-04-07 15:13:36 -0700

    Update reducers

--- src/reducers/forumReducers/offerReducer.js
+++ src/reducers/forumReducers/offerReducer.js
@@ -5,8 +5,9 @@ export default function(state = {}, action) {
     const { offerId, userId, title, items, message } = action;
 
     if(action.type === actionTypes.CREATE_OFFER) {
-        let newOfferId = shortid.generate();
-        return state.offers.concat({ newOfferId, userId, title, items, message });
+        let newOffers = state.offers.concat({ newOfferId, userId, title, items, message });
+        localStorage.setItem("offers", JSON.stringify(newOffers));
+        return newOffers;
     }
 
     if(action.type === actionTypes.EDIT_OFFER) {
@@ -17,6 +18,14 @@ export default function(state = {}, action) {
 
     if(action.type === actionTypes.DELETE_OFFER) {
         return state.filter(offer => offer.id !== offerId)
+    }
+
+    if(action.type === actionTypes.FETCH_OFFERS) {
+        if (localStorage.offers) {
+            return JSON.parse(localStorage.offers)
+          } else {
+            return "There are no offers";
+          }
     }
 
     return state;
